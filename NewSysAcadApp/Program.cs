41049a06bbfaabe7baf4053ac7c4f6b91264ef74
Mariano D'Angelo <mariano.dangelo@claro.com.ar>
2023-09-12 12:18:30 -0300

    added documents abuth the project

---
+++ NewSysAcadApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NewSysAcadApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
